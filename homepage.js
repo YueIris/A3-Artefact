50ee28b7396283b28aac1759951d10c5e39c0019
YueIris <73527420+YueIris@users.noreply.github.com>
2020-10-27 22:00:37 +1100

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -43,10 +43,7 @@ window.onload = function () {
     var timer;
     console.log('number',-listImg[0].width*(listImg.length-1));
 
-//style为类型，2为小圆点,1为左右点击
     function animate(offset,style) {
-//获取的是style.left，是相对左边获取距离，所以第一张图后style.left都为负值，
-//且style.left获取的是字符串，需要用parseInt()取整转化为数字。
         var newLeft
         if(style==1){
             newLeft = parseInt(list.style.left) + offset;
@@ -55,11 +52,11 @@ window.onload = function () {
             newLeft = offset;
         }
         list.style.left = newLeft + 'px';
-//无限滚动判断 +600 左
+//Infinite Scrolling, +600 left
         if (newLeft >0) {
             list.style.left = -listImg[0].width*(listImg.length-1) + 'px';
         }
-//-600 右
+//-600 right
         if (newLeft < -listImg[0].width*(listImg.length-1)){
             list.style.left = 0 + 'px';
         }
@@ -67,7 +64,7 @@ window.onload = function () {
 
 
     function buttonsShow(num) {
-//将之前的小圆点的样式清除
+//remove dots before
         for (var i = 0; i < buttons.length; i++) {
             if (buttons[i].className == "on") {
                 buttons[i].className = "";
@@ -81,7 +78,7 @@ window.onload = function () {
 
     }
 
-//左(没法移回去)
+//move back to left
     prev.onclick = function () {
         if(Public==0){
             Public=Public+listImg.length;
@@ -90,7 +87,7 @@ window.onload = function () {
         Public=Public-1;
         buttonsShow(Public);
     };
-//右
+//move to right
     next.onclick = function () {
         if(Public==listImg.length-1){
             Public=Public-listImg.length;
